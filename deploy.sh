48cd073190af7c433d4f4c37771be5379fe062d1
Sadman Tariq <sadmantariq@gmail.com>
2021-09-07 15:42:26 +0600

    Make deploy script push to existing repo.

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,17 @@
 set -e
 
 REPO=git@github.com:SadmanTariq/sadmantariq.github.io.git
+BRANCH=gh-pages
+
+if [ ! -d 'dist/.git' ]; then
+    if [ -d 'dist' ]; then
+        # if folder already exists then delete it
+        rm -r 'dist'
+    fi
+
+    # if repo doesnt exist then clone it
+    git clone "$REPO" -b "$BRANCH" 'dist'
+fi
 
 # build
 npm run build
@@ -11,9 +22,8 @@ npm run build
 # navigate into the build output directory
 cd dist
 
-git init
 git add -A
 git commit -m 'deploy'
 
-git push -f $REPO master:gh-pages
+git push
 cd -
